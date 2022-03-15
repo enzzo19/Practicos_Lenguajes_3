409910e9ef7986c3fdd64a64f26fba2a9f342a10
Juarez Enzo <49879701+enzzo19@users.noreply.github.com>
2022-03-15 03:56:07 -0300

    add TP_01

--- TP1_Juarez_Enzo/TP1_Juarez_Enzo/Form1.cs
+++ TP1_Juarez_Enzo/TP1_Juarez_Enzo/Form1.cs
@@ -16,5 +16,50 @@ namespace TP1_Juarez_Enzo
         {
             InitializeComponent();
         }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Enter))
+            {
+                textBox2.Focus();
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Enter))
+            {
+                textBox3.Focus();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                if (listBox1.GetSelected(i) == true)
+                {
+                    listBox1.SetSelected(i, true);
+                    MessageBox.Show(listBox1.Items[i].ToString());
+                }
+                else
+                {
+                    listBox1.SetSelected(i, false);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
